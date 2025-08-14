f8b95d1e98adea7c46e9f7d2e8b9fd982c69e752
Travis Hollins <87035520+tmomonet@users.noreply.github.com>
2025-08-14 11:53:52 -0600

    added community solutions

--- bwt.py
+++ bwt.py
@@ -24,3 +24,17 @@ def decode(s, n):
     for _ in range(lengthS):
         results = sorted([lst[i] + results[i] for i in range(lengthS)])
     return results[n]
+
+# ### Top Community Solution
+# def encode(s):
+#     lst = sorted( s[i or len(s):] + s[:i or len(s)] for i in reversed(range(len(s))) )
+#     return ''.join(ss[-1] for ss in lst), s and lst.index(s) or 0
+
+# def decode(s, n):
+#     out, lst = [], sorted((c,i) for i,c in enumerate(s))
+#     for _ in range(len(s)):
+#         c,n = lst[n]
+#         out.append(c)
+#     return ''.join(out)
+# note the use of slicing to handle rotation
+# also list comprehension
